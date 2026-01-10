911ed7e3f2720d93586702154ecaf5db6dfe0214
Vedant Dhande <veddhande7@gmail.com>
2026-01-10 11:34:58 +0530

    hello

--- day20_jan9/to-do/src/Todo.jsx
+++ day20_jan9/to-do/src/Todo.jsx
@@ -4,7 +4,7 @@ function Todo(){
 
 
 
-    const [taskList,upTaskList] = useState(["hello"])
+    const [taskList,upTaskList] = useState(["Eat breakfast","Water the plants"])
     const[newTask,upNewtask] = useState("")
 
     const addHandler=(event)=>{
@@ -15,24 +15,44 @@ function Todo(){
         upTaskList(t=>[...t,newTask])
     }
 
+    const deleteHandler=(index)=>{
+        const updatedTasks = taskList.filter((_ , i)=> i !== index)
+        upTaskList(updatedTasks)
+    }
+    const moveUp=(index)=>{
+        if(index>0){
+            const updatedTasks = [...taskList];
+            [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]];
+            upTaskList(updatedTasks)
+        }
+    }
+    const moveDown=(index)=>{
+        if(index<taskList.length){
+            const updatedTasks = [...taskList];
+            [updatedTasks[index],updatedTasks[index+1]] = [updatedTasks[index+1],updatedTasks[index]];
+            upTaskList(updatedTasks)
+        }
+    }
+
     return(
         <>
         <h1>Todo List</h1>
+        <div className="new-task">
         <input type="text" 
             placeholder="Enter a task..."
             value={newTask}
             onChange={addHandler}
                 />
         <button onClick={newTaskHandler}>Add</button><br />
-
+        </div>
         <div>
             <ol>
-                {taskList.map((task,index)=><>
+                {taskList.map((task,index)=><div className="one-task"> 
                 <li key={index}>{task}</li>
-                <button>❌</button>
-                <button>⬆️</button>
-                <button>⬇️</button>
-                </>
+                <button onClick={()=>deleteHandler(index)}>❌</button>
+                <button onClick={()=>moveUp(index)}>⬆️</button>
+                <button onClick={()=>moveDown(index)}>⬇️</button>
+                </div> 
                 )}
             </ol>
         </div>
